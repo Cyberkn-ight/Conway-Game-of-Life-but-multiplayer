c13f801ce5c46a8a30843e7b403fb01a9693591d
Cyberkn-ight <108725023+Cyberkn-ight@users.noreply.github.com>
2022-12-28 23:27:51 +0100

    Update GoLo
    Update so if fucking work

--- GoLo.py
+++ GoLo.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import msvcrt
 
 # Parse the arguments
 ip = sys.argv[1]
@@ -9,79 +10,36 @@ port = int(sys.argv[2])
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((ip, port))
 
+# Receive the initial grid from the server
+data = client_socket.recv(1024).decode().split('\n')
+n = int(data[0])
+grid = [[int(cell) for cell in row.split()] for row in data[1:]]
+
+# Print the initial grid
+print('\n'.join([' '.join(map(str, row)) for row in grid]))
+
 # Iterate through the grid and apply the rules of the Game of Life
 while True:
-    # Receive the grid from the server
-    data = client_socket.recv(1024).decode()
-    if not data:
-        break
-    n, *rows = data.split('\n')
-    n = int(n)
-    grid = [[int(cell) for cell in row.split()] for row in rows if row]
+    # Check if the user wants to pause the game
+    if msvcrt.kbhit() and msvcrt.getch() == b' ':
+        # Send the pause instruction to the server
+        client_socket.send(bytes("p\n", "utf-8"))
 
-    # Print the grid
-    for row in grid:
-        print(' '.join([str(cell) for cell in row]))
-    print()
+        # Receive the updated grid from the server
+        data = client_socket.recv(1024).decode().split('\n')
+        grid = [[int(cell) for cell in row.split()] for row in data[:-1]]
 
-    # Get a move from the user
-    move = input("Enter a move (x,y) or 'p' to pause: ")
+        # Print the updated grid
+        print('\n'.join([' '.join(map(str, row)) for row in grid]))
 
-    # If the user wants to pause the game, disconnect from the server and enter single player mode
-    if move == 'p':
-        client_socket.close()
-        while True:
-            # Get a move from the user
-            move = input("Enter a move (x,y) or 'c' to connect to a server: ")
+    # Check if the user wants to place a cell
+    elif msvcrt.kbhit() and msvcrt.getch() == b'e':
+        # Prompt the user to enter the coordinates of the cell
+        x, y = map(int, input("Enter the coordinates (x,y) of the cell: ").split(','))
 
-            # If the user wants to connect to a server, reconnect to the server and continue the game
-            if move == 'c':
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect((ip, port))
-                break
-
-            # Parse the move
-            x, y = map(int, move.split(','))
-
-            # Update the grid
-            grid[x][y] = 1
-
-            # Count the number of live neighbors for each cell
-            neighbors = [[0 for _ in range(n)] for _ in range(n)]
-            for i in range(n):
-                for j in range(n):
-                    if i > 0:
-                        neighbors[i][j] += grid[i-1][j]
-                    if i < n-1:
-                        neighbors[i][j] += grid[i+1][j]
-                    if j > 0:
-                        neighbors[i][j] += grid[i][j-1]
-                    if j < n-1:
-                        neighbors[i][j] += grid[i][j+1]
-                    if i > 0 and j > 0:
-                        neighbors[i][j] += grid[i-1][j-1]
-                    if i < n-1 and j < n-1:
-                        neighbors[i][j] += grid[i+1][j+1]
-                    if i > 0 and j < n-1:
-                        neighbors[i][j] += grid[i-1][j+1]
-                    if i < n-1 and j > 0:
-                        neighbors[i][j] += grid[i+1][j-1]
-
-            # Update the grid based on the number of live neighbors
-            for i in range(n):
-                for j in range(n):
-                    if grid[i][j] == 0 and neighbors[i][j] == 3:
-                        grid[i][j] = 1
-                    elif grid[i][j] == 1 and (neighbors[i][j] < 2 or neighbors[i][j] > 3):
-                        grid[i][j] = 0
-
-            # Print the grid
-            for row in grid:
-                print(' '.join([str(cell) for cell in row]))
-            print()
-    else:
-        # Send the move to the server
-        client_socket.send(bytes(move, "utf-8"))
+        # Send the placement instruction and the coordinates to the server
+        client_socket.send(bytes("e\n", "utf-8"))
+        client_socket.send(bytes(f"{x},{y}\n", "utf-8"))
 
 # Close the socket
 client_socket.close()
